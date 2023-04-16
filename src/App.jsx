0a0da7817f554174db49c6c9a31d542778ee230c
takeo-t <takeo-t@118satellite.com>
2023-04-16 22:25:59 +0900

    検索機能

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
-import { SerchedStations } from './components/SerchedStations';
-import { ShowStation } from './components/ShowStation';
+// import { SerchedStations } from './components/SerchedStations';
+// import { ShowStation } from './components/ShowStation';
 import StationsData from './StationsData.json';
 
 
-const StationTitle = StationsData.StationsData;
-const stations = [];
-stations.push(StationTitle);
+const stations = StationsData.StationsData;
 console.log(stations);
 
 export const App = () => {
   const [ showStations, setShowStations ] = useState(stations);//初期値は配列stations
   const [ inputValue, setInputValue ] = useState();
+
 
   //検索欄への入力値をハンドリング
   const handleInputChange = (event) => {
@@ -26,24 +25,42 @@ export const App = () => {
       setShowStations(stations);
       return;
     }
-
     const serchedStations = stations.filter(//フィルター関数は条件に一致する値のみ配列の中から取り出すことができる
       (station) => Object.values(station).filter(
         (item) =>
         item !== undefined &&
         item !== null &&
-        item.toString().toUpperCase().indexOf(value.toUpperCase()) !== -1
+        item === station.stationName &&
+        item === value &&
+        item.toUpperCase().indexOf(value.toUpperCase()) !== -1
       ).length > 0
     );
     setShowStations(serchedStations);//与える引数はserchedStations　serchedStationsには検索結果の配列が入っている。
+  
   }
-  console.log(showStations);
-
   return (
     <>
     <h1>名鉄名古屋駅乗車位置検索</h1>
-    <SerchedStations inputValue={inputValue} onChange={handleInputChange}/>
-    <ShowStation />
+    <div>
+        <h4>降りる駅を入力</h4>
+        <input type="text" value={inputValue} placeholder="駅名を入力" onChange={handleInputChange}/>
+      </div>
+    {showStations.map((station, index) => {
+      if(showStations.length >= 2){
+      return null;
+      } else {
+      return (
+        <div key={index}>
+          <p>{station.stationName}駅（{station.stationNum}）へ向かう時の並び位置</p>
+          <ul>
+            <li>{station.trackNum}番ホーム</li>
+            <li>{station.position}</li>
+            <li>{station.positionColor}色乗車位置</li>
+          </ul>
+        </div>
+      )
+      }
+    })}
   </>
   );
 };
--- src/components/SerchedStations.jsx
+++ src/components/SerchedStations.jsx
@@ -8,7 +8,7 @@ export const SerchedStations = (props) => {
    <div>
       <h3>あなたの向かう駅は？</h3>
       <input type="text" placeholder="駅名を入力" value={inputValue} onChange={onChange}/>
-      <button onClick={SerchedStations}>検索</button>
+      {/* <button onClick={SerchedStations}>検索</button> */}
    </div>
    </>
     );
